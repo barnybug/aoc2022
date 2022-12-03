2936348c5710807cad76cbd0ac660c815438e8df
Barnaby Gray <barnaby@pickle.me.uk>
2022-12-03 12:56:27 +0000

    Markdown output

--- aoc2022/run.py
+++ aoc2022/run.py
@@ -4,9 +4,33 @@ from pathlib import Path
 
 from .utils import input_data
 
+class MarkdownOutput:
+    def header(self):
+        print("# Solutions")
+        print()
+
+    def day(self, day, answer):
+        print(f"## Day {day}")
+        print()
+        print(f"- Part 1: {answer.part1}")
+        print(f"- Part 2: {answer.part2}")
+        print()
+
+class TextOutput:
+    def header(self):
+        print("Solutions")
+
+    def day(self, day, answer):
+        print(f"Day {day}:")
+        print(answer)
+        print()
+
+# run: echo '### Hello world! :rocket:' >> $GITHUB_STEP_SUMMARY
+
 def run():
     parser = argparse.ArgumentParser()
     parser.add_argument("day", nargs="*", type=int)
+    parser.add_argument("--markdown", action="store_true", help="Format output in markdown")
     args = parser.parse_args()
 
     if args.day:
@@ -18,10 +42,13 @@ def run():
             for path in paths
         ]
         days.sort()
-        
+
+    output = MarkdownOutput() if args.markdown else TextOutput()
+    output.header()
+
     for day in days:
         name = f"aoc2022.day{day:02d}"
         mod = importlib.import_module(name)
         input = input_data(day)
-        print(f"Day {day}:")
-        print(mod.solve(input))
+        answer = mod.solve(input)
+        output.day(day, answer)
